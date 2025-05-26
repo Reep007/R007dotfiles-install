6dd3eb080737437b218591065ce8d6ce1f0f6860
Reep <58405387+Reep007@users.noreply.github.com>
2025-05-26 14:21:32 -0400

    Update install-dotfiles.sh

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -28,7 +28,7 @@ info "Installing official Arch Linux packages..."
 sudo pacman -S --noconfirm \
   hyprland waybar hyprpaper python xdg-desktop-portal-hyprland kitty lsd zsh zsh-completions btop python-pillow python tk \
   thunar thunar-archive-plugin tumbler wofi rofi dunst python-pywal python-gobject xdg-user-dirs gtk3 \
-  qt5ct network-manager-applet pavucontrol jq nodejs npm pacman-contrib xf86-video-amdgpu \
+  qt5ct network-manager-applet jq nodejs npm pacman-contrib xf86-video-amdgpu \
   gvfs gvfs-mtp gvfs-smb gvfs-nfs gvfs-gphoto2 gvfs-afc polkit polkit-gnome nvidia nvidia-utils \
   mpv nano ttf-jetbrains-mono-nerd wl-clipboard grim slurp fd lxappearance
 
